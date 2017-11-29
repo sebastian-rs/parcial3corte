208c2727e6879d1a478fe01879fda0bccfeaba35
JCBOT <JCBOT@172.25.7.109>
2017-11-28 21:24:30 -0500

    Shemaultima

--- src/main/java/Controlador/Principal.java
+++ src/main/java/Controlador/Principal.java
@@ -70,26 +70,26 @@ public class Principal extends HttpServlet {
 
             if (entrar == 2) {
                 
-                Crud_Usuarios uss = new Crud_Usuarios();
-                List<principale> index1 = null;
-             
-                try {
-                    index1 = uss.findAll();
-                } catch (SQLException ex) {
-                    Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                
-
-                for (int i = 0; i < index1.size(); i++) {
-
-                    if (index1.get(i).getContrasena().equals(contrasena)) {
+//                Crud_Usuarios uss = new Crud_Usuarios();
+//                List<principale> index1 = null;
+//             
+//                try {
+//                    index1 = uss.findAll();
+//                } catch (SQLException ex) {
+//                    Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//                
+//
+//                for (int i = 0; i < index1.size(); i++) {
+//
+//                    if (index1.get(i).getContrasena().equals(contrasena)) {
                         request.getRequestDispatcher("Application.jsp").forward(request, response);
-                    }
-
-                }
-
-                String message = "El usuario no esta registrado en la base de datos";
-                request.setAttribute("message", message);
+//                    }
+//
+//                }
+//
+//                String message = "El usuario no esta registrado en la base de datos";
+//                request.setAttribute("message", message);
                 request.getRequestDispatcher("principal.jsp").forward(request, response);
 
             }
